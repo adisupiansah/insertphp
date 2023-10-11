eaa9034abc56d1c86b6ba600ecd676465c4924a8
kuizteknikinformatika <adisupiansah15@gmail.com>
2023-10-11 08:51:06 +0700

    update

--- input.php
+++ input.php
@@ -13,7 +13,7 @@ $daftar = query ("SELECT * FROM daftar");
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="input.css">
+    <link rel="stylesheet" type="text/css" href="css/input.css">
     <link rel="shortcut icon" href="img/logo.png">
 
 
--- login.php
+++ login.php
@@ -26,7 +26,7 @@ if (isset($_POST["submit"])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="login.css">
+    <link rel="stylesheet" type="text/css" href="css/login.css">
     <link rel="shortcut icon" href="img/logo.png">
 
 
